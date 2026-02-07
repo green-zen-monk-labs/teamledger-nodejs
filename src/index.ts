4dd55f109278921880f0a98cab104637f820221b
Zenyatta (Krisztián) <72669043+zenyatta90@users.noreply.github.com>
2026-02-07 01:18:05 +0100

    Typescript learn project

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+enum Currency {
+    HUF,
+    EUR,
+}
+
+type Money = {
+    amount: number;
+    currency: Currency;
+}
+
+type Transaction = {
+    id: string;
+    amount: Money;
+    category: string;
+    createdAt: Date;
+}
+
+function sumByCategory(transactions: Transaction[]): Record<string, number> {
+    const results: Record<string, number> = {}
+  
+    transactions.forEach(transaction => {
+        if (!results[transaction.category]) {
+            results[transaction.category] = 0;
+        }
+        results[transaction.category] += transaction.amount.amount;
+
+    });
+
+  return results;  
+}
+
+const testTransactions: Transaction[] = [
+    { id: "1", amount: { amount: 1000, currency: Currency.HUF }, category: "food", createdAt: new Date() },
+    { id: "2", amount: { amount: 500, currency: Currency.HUF }, category: "transport", createdAt: new Date() },
+    { id: "3", amount: { amount: 2000, currency: Currency.HUF }, category: "food", createdAt: new Date() },
+    { id: "4", amount: { amount: 1500, currency: Currency.HUF }, category: "entertainment", createdAt: new Date() },
+];
+
+console.log(sumByCategory(testTransactions));
